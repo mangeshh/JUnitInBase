c9b7f758adc1a7283e8c5e34f74ef2b65ebd5aaf
mangeshh <mangesh.hankare@gmail.com>
2023-11-25 14:35:01 -0500

    refactoring

--- JUnitInBase/src/test/java/com/example/sample/CalculatorParameterizedTest.java
+++ JUnitInBase/src/test/java/com/example/sample/CalculatorParameterizedTest.java
@@ -1,11 +1,13 @@
 package com.example.sample;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * learn parameterized tests.
+ */
 public class CalculatorParameterizedTest {
 
 
