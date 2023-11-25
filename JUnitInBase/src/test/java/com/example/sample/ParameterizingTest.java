b521be027e181aa94762dab44942cb3210697db4
mangeshh <mangesh.hankare@gmail.com>
2023-11-25 16:15:03 -0500

    refactoring

--- JUnitInBase/src/test/java/com/example/sample/ParameterizingTest.java
+++ JUnitInBase/src/test/java/com/example/sample/ParameterizingTest.java
@@ -1,27 +1,49 @@
 package com.example.sample;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-
-import java.util.stream.Stream;
-
+import org.junit.jupiter.params.provider.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+/**
+ * @ValueSource
+ * @CsvSource
+ * @MethodSource
+ * @CsvFileSource
+ *
+ * Note - we covered all scenarios to parameterized class.
+ *
+ */
 public class ParameterizingTest {
 
     @ParameterizedTest
-    @MethodSource("stringProvider")
+    @ValueSource(ints = {1, 2, 3, 4, 5})
+    void testWithValueSource(int value) {
+        // Test code using the 'value' parameter
+    }
+
+    @ParameterizedTest
+    @CsvSource({"1, 2, 3", "0, 0, 0", "-1, 1, 0", "10, -5, 5"})
+    void testAddition(int num1, int num2, int expectedSum) {
+        Calculator calculator = new Calculator();
+        int actualSum = calculator.add(num1, num2);
+        assertEquals(expectedSum, actualSum, () -> num1 + " + " + num2 + " should equal " + expectedSum);
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("com.example.sample.Util#stringProvider") // ClassName#methodName.
     void testStringLength(String input, int expectedLength) {
         assertEquals(expectedLength, input.length());
     }
 
-    static Stream<Arguments> stringProvider() {
-        return Stream.of(
-                Arguments.of("hello", 5), //input, expectedLength
-                Arguments.of("JUnit", 5),
-                Arguments.of("parameterized", 12)
-        );
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/test-data.csv", numLinesToSkip = 1) // Provide the correct path to your CSV file
+    void testWithCsvFileSource(String param1, int param2, boolean param3) {
+        // Test code using the parameters from the CSV file
+        System.out.println("param1: " + param1);
+        System.out.println("param2: " + param2);
+        System.out.println("param3: " + param3);
     }
 
 }
