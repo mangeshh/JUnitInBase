a0862318116780ae2d292f124680bd882a8e6847
mangeshh <mangesh.hankare@gmail.com>
2023-11-26 15:49:45 -0500

    refactoring

--- JUnitInBase/src/test/java/com/mockito/sample/argument_captor/UserServiceTest.java
+++ JUnitInBase/src/test/java/com/mockito/sample/argument_captor/UserServiceTest.java
@@ -24,7 +24,10 @@ public class UserServiceTest {
         ArgumentCaptor<String> nameCaptor = ArgumentCaptor.forClass(String.class);
         ArgumentCaptor<Integer> ageCaptor = ArgumentCaptor.forClass(Integer.class);
 
-        // Capture the arguments
+        /** below verify and createUser does what?
+         * Method Invocation Verification: It verifies that the createUser method of the mockUserService object was called during the execution of your test. In other words, it checks if your code interacts with the mockUserService by invoking its createUser method.
+         * Argument Capturing: It captures the values of the name and age arguments passed to the createUser method when it was called.
+         */
         verify(mockUserService).createUser(nameCaptor.capture(), ageCaptor.capture());
 
         // Assert that the captured values are the ones we expect
