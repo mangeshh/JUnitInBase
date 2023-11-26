70c7055c3959a7081f49d48a1453f625befe54e6
mangeshh <mangesh.hankare@gmail.com>
2023-11-26 15:37:53 -0500

    refactoring

--- JUnitInBase/src/test/java/com/mockito/sample/Num_of_Invocations_with_Arguments/SimpleUsageOfVerifyExample.java
+++ JUnitInBase/src/test/java/com/mockito/sample/Num_of_Invocations_with_Arguments/SimpleUsageOfVerifyExample.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import static org.mockito.Mockito.*;
 
-public class SimpleExample {
+public class SimpleUsageOfVerifyExample {
 
     @Test
     public void test(){
@@ -22,7 +22,9 @@ public class SimpleExample {
         mockedList.add("three times");
         mockedList.add("three times");
 
-        verify(mockedList, times(1)).add("once");
+        verify(mockedList).add("once"); // it verifies the method was called.
+
+        verify(mockedList, times(1)).add("once"); // it verifies the method was call with specific number of times.
         verify(mockedList, times(2)).add("twice");
         verify(mockedList, times(3)).add("three times");
     }
